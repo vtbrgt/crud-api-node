1533a1c27524e64956600ce8531c63e0b3e27ec9
Vitor Borgatte <vitor.borgatte@icloud.com>
2023-02-17 10:42:27 -0300

    descomentando código em app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ app.use(express.json());
 import router from './routes.js';
 app.use(router);
 
-// import {createTableFuncionarios} from './Models/Funcionarios.js'
-// createTableFuncionarios()
+import { createTableFuncionarios } from './Models/Funcionarios.js';
+createTableFuncionarios();
 
 app.listen(3000, () => console.log('API Rodando'));
